d2cee49951d777938e4151793f927abe332a9dfc
Tecnología Eilyn <ingcristianq@gmail.com>
2024-09-24 11:01:30 -0500

    primer commit

---
+++ install.py
@@ -0,0 +1,16 @@
+print ("hola mundo")
+nombre = "xtentationx"
+apellido = "server"
+nombreCompleto = "hola " + nombre + " " + apellido
+print(nombreCompleto)
+print(type(nombre))
+edad = 31
+edad += 1
+# print (type (edad))
+
+altura = 1.74
+print (type (altura))
+# print ("tu altura es : " + str(altura))
+print (edad)
+humano = False
+print ("eres un humano: " + str(humano))
